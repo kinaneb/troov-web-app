66c443c25916eab6a2b5b295b940df05d9958b40
Kinan <kinanb@live.com>
2024-06-12 13:45:35 +0200

    feat: adding monodb service to the docker compose with the connection test to the basic endpoint

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 // index.js
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 
 dotenv.config();
 
 const app = express();
+const mongoURI = process.env.MONGO_URI || 'mongodb://mongo:27017/mydatabase';
+
+async function connectDB() {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('MongoDB connected successfully ...');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+}
+
+connectDB();
+
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
